7535403007efbf60d89ca6376362bdb4d80a2353
Thiseas C. Lamnidis <thisseass@gmail.com>
2023-03-13 17:22:36 +0100

    local variables in all functions. 2 new functions

--- scripts/source_me.sh
+++ scripts/source_me.sh
@@ -4,9 +4,14 @@
 #   errecho -r will print in red
 #   errecho -y will print in yellow
 function errecho() {
-  local Normal=$(tput sgr0)
-  local Red=$(tput sgr0)'\033[1;31m' ## Red normal face
-  local Yellow=$(tput sgr0)'\033[1;33m' ## Yellow normal face
+  local Normal
+  local Red
+  local Yellow
+  local colour
+
+  Normal=$(tput sgr0)
+  Red=$(tput sgr0)'\033[1;31m' ## Red normal face
+  Yellow=$(tput sgr0)'\033[1;33m' ## Yellow normal face
 
   colour=''
   if [[ ${1} == '-y' ]]; then
@@ -28,16 +33,21 @@ function check_fail() {
 }
 
 ## Function to return index of item in bash array
+## usage: get_index_of <value> "<array>" ## Array call MUST be quoted
 #   i='banana'
 #   list=("banana split" "apple" "potato" "banana" "apple")
-#   get_index_of ${i} "${list[@]}" ## Array MUST be quoted
+#   get_index_of ${i} "${list[@]}" ## returns 3 ('banana split' =/= 'banana')
 #   Returns: a list of all indexes that match the value.
 function get_index_of() {
   local i
-  local value=${1}
+  local value
+  local array
+  local indices
+
+  value=${1}
   shift 1
-  local array=("${@}")
-  local indices=''
+  array=("${@}")
+  indices=''
 
   for i in "${!array[@]}"; do
     if [[ "${array[${i}]}" == "${value}" ]]; then
@@ -54,10 +64,15 @@ function get_index_of() {
 #   Returns: The number of times a value was found in the array. No partial matching (i.e. 'banana' will not match 'banana split')
 function count_instances() {
   local i
-  local value="${1}"
+  local value
+  local array
+  local result
+  local i
+  
+  value="${1}"
   shift 1
-  local array=("${@}")
-  local let result=0
+  array=("${@}")
+  let result=0
 
   for i in "${array[@]}"; do
     if [[ "${i}" == "${value}" ]]; then
@@ -67,18 +82,58 @@ function count_instances() {
   echo ${result}
 }
 
+## Function to return the number of entries in a field, given a specified delimiter
+##  <delim> MUST be a single character
+# usage: number_of_entries <delim> <field_contents>
+# number_of_entries ';' 'ABC001;ABC001_subset' ## Returns 2
+function number_of_entries() {
+  local delim
+  local value
+  local result
+
+  delim="${1}"
+  value=($(echo ${2} | cut --output-delimiter ' ' -d ${delim} -f 1- ))
+  let result=${#value[@]}
+
+  echo ${result}
+}
+
+## Function to return the requested value from a list column, by index
+##  <delim> MUST be a single character
+##  <index> is 0-based
+# usage: pull_by_index <delim> <field_contents> <index>
+# number_of_entries ';' 'ABC001;ABC001_subset' 1 ## Returns 'ABC001'
+function pull_by_index() {
+  local delim
+  local value
+  local index
+  
+  delim="${1}"
+  value=($(echo ${2} | cut --output-delimiter ' ' -d ${delim} -f 1- ))
+  let index=${3}
+
+  echo ${value[${index}]}
+}
+
 ## Function to check if all items of array X are in array Y, and return any entries exclusive to X.
 #   all_x_in_y <length_of_list_x> X1 [X2 X3 ...] <length_of_list_y> Y1 [Y2 Y3 ...]
 #   Returns: A space separated list of exclusive elements in X
 function all_x_in_y() {
-  local let length_x=${1}
+  local length_x
+  local x
+  local length_y
+  local y
+  local exclusive_entries
+  local xid
+
+  let length_x=${1}
   shift 1
-  local x=("${@:1:${length_x}}")
+  x=("${@:1:${length_x}}")
   shift ${length_x}
-  local let length_y=${1}
+  let length_y=${1}
   shift 1
-  local y=("${@:1:${length_y}}")
-  local exclusive_entries=()
+  y=("${@:1:${length_y}}")
+  exclusive_entries=()
 
   for xid in ${x[@]}; do
     if [[ $(count_instances ${xid} "${y[@]}") == 0 ]]; then
@@ -93,9 +148,13 @@ function all_x_in_y() {
 # usage: infer_library_udg 'half;minus;plus' 0
 # The second argument (index) is 0-based
 function infer_library_udg() {
-  local value=($(echo ${1} | cut --output-delimiter ' ' -d ';' -f 1- ))
-  local let index=${2}
+  local value
+  local index
   local result
+
+  value=($(echo ${1} | cut --output-delimiter ' ' -d ';' -f 1- ))
+  let index=${2}
+  result
 
   if [[ ${#value[@]} == '1' ]]; then
       if [[ ${value[0]} == 'mixed' ]]; then
@@ -130,9 +189,12 @@ function infer_library_udg() {
 # usage: infer_library_udg 'ds;ds;ss' 0
 # The second argument (index) is 0-based
 function infer_library_strandedness() {
-  local value=( $(echo "${1}" | cut -d ';' --output-delimiter " " -f 1-) )
-  local let index=${2}
+  local value
+  local index
   local result
+  
+  value=( $(echo "${1}" | cut -d ';' --output-delimiter " " -f 1-) )
+  let index=${2}
 
   if [[ ${#value[@]} == '1' ]]; then
       if [[ ${value[0]} == 'other' ]]; then
@@ -163,11 +225,17 @@ function infer_library_strandedness() {
 #   usage: r1_r2_from_ena_fastq ${path_to_ena_data} ${fastq_fn}
 #   Returns: a thrupple of: seq_type R1 R2
 function r1_r2_from_ena_fastq() {
-  local data_path=$1
-  local value=$2
-  local r1="${data_path}/${value%%;*}"
-  local r2="${data_path}/${value##*;}"
-  local seq_type="PE"
+  local data_path
+  local value
+  local r1
+  local r2
+  local seq_type
+
+  data_path=$1
+  value=$2
+  r1="${data_path}/${value%%;*}"
+  r2="${data_path}/${value##*;}"
+  seq_type="PE"
 
   if [[ "${r2}" == "${r1}" ]]; then
     r2="NA"
@@ -181,13 +249,20 @@ function r1_r2_from_ena_fastq() {
 #   Usage: infer_colour_chemistry ${instrument_platform} ${instrument_model}
 #   Returns: The colour chemistry for the specified sequencer. Throw error and stop if the platform or sequencer are not identified.
 function infer_colour_chemistry() {
-  local platform=${1}
-  local model=${2}
+  local platform
+  local model
+  local one_chem_seqs
+  local two_chem_seqs
+  local four_chem_seqs
+  local colour_chemistry
+
+  platform=${1}
+  model=${2}
   ## Hard-coded list of sequencers per colour chemistry
-  local one_chem_seqs=("Illumina iSeq 100") ## Not sure eager can process this, but good to have a record of it.
-  local two_chem_seqs=("NextSeq 1000" "NextSeq 500" "NextSeq 550" "Illumina NovaSeq 6000" "Illumina MiniSeq")
-  local four_chem_seqs=("Illumina HiSeq 1000" "Illumina HiSeq 1500" "Illumina HiSeq 2000" "Illumina HiSeq 2500" "Illumina HiSeq 3000" "Illumina HiSeq 4000" "HiSeq X Five" "HiSeq X Ten" "Illumina Genome Analyzer" "Illumina Genome Analyzer II" "Illumina Genome Analyzer IIx" "Illumina HiScanSQ" "Illumina MiSeq")
-  local colour_chemistry=''
+  one_chem_seqs=("Illumina iSeq 100") ## Not sure eager can process this, but good to have a record of it.
+  two_chem_seqs=("NextSeq 1000" "NextSeq 500" "NextSeq 550" "Illumina NovaSeq 6000" "Illumina MiniSeq")
+  four_chem_seqs=("Illumina HiSeq 1000" "Illumina HiSeq 1500" "Illumina HiSeq 2000" "Illumina HiSeq 2500" "Illumina HiSeq 3000" "Illumina HiSeq 4000" "HiSeq X Five" "HiSeq X Ten" "Illumina Genome Analyzer" "Illumina Genome Analyzer II" "Illumina Genome Analyzer IIx" "Illumina HiScanSQ" "Illumina MiSeq")
+  colour_chemistry=''
 
   ## Throw an error if sequencer is not ILLUMINA
   if [[ ${platform} != "ILLUMINA" ]]; then

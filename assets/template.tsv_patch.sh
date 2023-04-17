d6be8ccf65743e0b0f7e5b55efec555b08bbadac
Thiseas C. Lamnidis <thisseass@gmail.com>
2023-04-17 17:36:51 +0200

    keep only needed columns. version tracking

--- assets/template.tsv_patch.sh
+++ assets/template.tsv_patch.sh
@@ -14,8 +14,30 @@ VERSION='0.1.0dev'
 local_data_dir="$(readlink -f ${1})"
 input_tsv="$(readlink -f ${2})"
 output_tsv="$(dirname ${local_data_dir})/$(basename -s ".tsv" ${input_tsv}).finalised.tsv"
+columns_to_keep=("Sample_Name" "Library_ID" "Lane" "Colour_Chemistry" "SeqType" "Organism" "Strandedness" "UDG_Treatment" "R1" "R2" "BAM")
+source $(dirname ${2})/../../scripts/source_me.sh ## Load helper functions
 
-sed -e "s|<PATH_TO_DATA>|${local_data_dir}|g" ${input_tsv} > ${output_tsv}
+## Index non-proliferated columns and exclude them from the finalised TSV
+cut_selector=''
+tsv_header=($(head -n1 ${input_tsv}))
+for col_name in ${columns_to_keep[@]}; do
+  let idx=$(get_index_of ${col_name} "${columns_to_keep[@]}")+1 ## awk uses 1-based indexing
+  if [[ ! ${idx} -eq -1 ]]; then
+    cut_selector+="${idx},"
+  fi
+done
+
+## Remove added columns, and put columns in right order
+cut -f ${cut_selector%,} ${input_tsv} > ${output_tsv}
+sed -i -e "s|<PATH_TO_DATA>|${local_data_dir}|g" ${output_tsv}
 
 ## Any further commands to edit the file before finalisation should be added below as shown
 # sed -ie 's/replace_this/with_this/g' ${output_tsv}
+
+## Keep track of versions
+##    Remove versions from older run if there
+grep -v -F -e "$(basename ${0})" -e "source_me.sh for final TSV" ${version_file} >${version_file}.new
+##    Then add new versions
+echo -e "$(basename ${0}):\t${VERSION}" >> ${version_file}.new
+echo -e "source_me.sh for final TSV:\t${HELPER_FUNCTION_VERSION}" >>${version_file}.new
+mv ${version_file}.new ${version_file}

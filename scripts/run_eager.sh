f98c5e3b0d281c6b1cde5a682c1a57b0928a9802
Thiseas Christos Lamnidis <thisseass@gmail.com>
2024-04-16 17:39:04 +0200

    use eager 2.5.1

--- scripts/run_eager.sh
+++ scripts/run_eager.sh
@@ -53,7 +53,7 @@ fi
 ## Hard-coded params
 # repo_dir='/mnt/archgen/poseidon/poseidon-eager'
 nxf_path="/mnt/archgen/tools/nextflow/22.04.5.5708/" ## Use centarlly installed version
-eager_version='2.4.6'
+eager_version='2.5.1'
 root_eager_dir="${local_poseidon_eager}/eager/"
 root_package_dir="${local_minotaur_recipes}/packages/"
 nextflow_profiles="eva,archgen,big_data,eva_local_paths"
